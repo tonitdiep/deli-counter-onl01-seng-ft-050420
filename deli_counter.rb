b558689e54a527b8e1177ba71608abd67e4dbe87
Toni  Diep <adiep123@gmail.com>
2020-05-06 04:33:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(array)
   current_line = "The Line is currently."
   if array.size > 1
     array.each do |person|
-      current_line << "#{array.index(person)+1}. #{person}""
+      current_line << "#{array.index(person)+1}. #{person}"
+      
 end
 
 def take_a_number(array, name)
