abfae85743b5ef26b8e0c37eec06f22f28441c07
Toni  Diep <adiep123@gmail.com>
2020-05-06 04:39:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(array)
+  current_line = "The line is currently:"
+  if array.size >= 1
+    array.each do |person|
+    current_line << " #{array.index(person)+1}. #{person}"
+  end
+    puts current_line
+  else
+      puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  number = array.length
+    puts "Welcome, #{name}. You are number #{number} in line."
+end
+
+def now_serving(array)
+  if array.length > 1
+    puts "Currently serving #{array.first}."
+    array.shift
+    else
+  puts "There is nobody waiting to be served!"
+  end
+end
